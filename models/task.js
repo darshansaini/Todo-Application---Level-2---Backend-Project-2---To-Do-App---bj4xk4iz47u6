46ad31af2666512508e9d2ccfd5b9df86482ab7a
Darshan Saini <50915527+darshansaini@users.noreply.github.com>
2023-06-23 19:07:08 +0530

    Update task.js

--- models/task.js
+++ models/task.js
@@ -14,8 +14,22 @@ Complete your other field here
 
 var taskSchema = mongoose.Schema({
   //Write your code here.
+  heading:{
+    type:String,
+    required:true},
+  description:{
+    type:String,
+    required:true},
+  status:{
+  type:String,
+  enum:['pending','done'],
+    default:'pending'},
+  creator_id:{
+  type: mongoose.Schema.Types.ObjectId,
+      ref: 'User'}
+  
 },
-{ /* ... */ }
+{ timestamps: true }
 );
 
 
